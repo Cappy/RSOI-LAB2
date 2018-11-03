5bbe271c5b2c5183c62c12b81b899d7091ebfa09
Pavel Kiselev <cappyru@gmail.com>
2018-11-03 19:52:20 +0300

    added logging

--- RoomsAPIServices/RoomsAPIServices/Controllers/RoomsController.cs
+++ RoomsAPIServices/RoomsAPIServices/Controllers/RoomsController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using RoomsAPIServices.Models;
 using PagedList;
+using Microsoft.Extensions.Logging;
 
 namespace RoomsAPIServices.Controllers
 {
